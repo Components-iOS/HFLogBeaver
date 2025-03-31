c4964d84b103b4bcb1c9d72dd23ba85012d77d6b
liuhongfei <hongfei_liu@bizconf.cn>
2025-03-31 15:32:02 +0800

    增加日志大小到达最大限制，重新生成日志文件逻

--- Example/HFLogBeaver/HFViewController.m
+++ Example/HFLogBeaver/HFViewController.m
@@ -24,12 +24,25 @@
     
     OCLogger *logger = [OCLogger defaultManager];
     logger.enableFileLog = YES;
+    logger.logFileMaxSize = 128 * 1024;
     [logger setupLogBeaver];
-    
+        
     HFLogInfo(@"%@", [logger getLogFilePath]);
     HFLogInfo(@"%@", [logger getLogPrefixName]);
+    
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
+    dispatch_source_set_timer(timer, DISPATCH_TIME_NOW, 0.5 * NSEC_PER_SEC, 0);
 
-    [self log:@"111" str1:@"222" str2:@"333"];
+    dispatch_source_set_event_handler(timer, ^{
+        for (int i = 0; i < 1000; i++) {
+            [self log:@"111" str1:@"222" str2:@"333"];
+        }
+    });
+
+    dispatch_resume(timer);
+    
+    [[NSRunLoop mainRunLoop] run];
 }
 
 - (void)log:(NSString *)str str1:(NSString *)str1 str2:(NSString *)str2 {
